35e03160bc1145e379b4aa43876696fa06df29f8
An The <at.honour13.hcmus@gmail.com>
2017-08-06 22:42:26 +0700

    [Done] Lecture 19 - PokeDashboard Advanced (PokemonModal)

---
+++ Lecture 19 - PokeDashboard Advance/pokedashboard/src/components/PokemonModal.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap/lib';
+
+const PokemonModal = React.createClass({
+	render() {
+		return(
+		<div>
+			<Button
+				bsStyle="primary"
+				bsSize="large"
+				onClick={this.props.openModal}>
+				Launch contained modal
+			</Button>
+
+			<Modal
+				show={this.props.showModal}
+				onHide={this.props.closeModal}>
+				<Modal.Header closeButton>
+					<Modal.Title id="contained-modal-title">Contained Modal</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					Elit est explicabo ipsum eaque dolorem blanditiis doloribus sed id ipsam, beatae, rem fuga id earum? Inventore et facilis obcaecati.
+				</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={this.props.closeModal}>Close</Button>
+				</Modal.Footer>
+			</Modal>
+		</div>
+	);
+	}
+})
+
+// const PokemonModal = ({toggleModal, showModal, pokemon}) => {
+// 	return(
+// 		<div>
+// 			<Button
+// 				bsStyle="primary"
+// 				bsSize="large"
+// 				onClick={toggleModal}>
+// 				Launch contained modal
+// 			</Button>
+
+// 			<Modal
+// 				show={showModal}
+// 				onHide={toggleModal}
+// 				container={this}
+// 				aria-labelledby="contained-modal-title">
+// 				<Modal.Header closeButton>
+// 					<Modal.Title id="contained-modal-title">Contained Modal</Modal.Title>
+// 				</Modal.Header>
+// 				<Modal.Body>
+// 					Elit est explicabo ipsum eaque dolorem blanditiis doloribus sed id ipsam, beatae, rem fuga id earum? Inventore et facilis obcaecati.
+// 				</Modal.Body>
+// 				<Modal.Footer>
+// 					<Button onClick={toggleModal}>Close</Button>
+// 				</Modal.Footer>
+// 			</Modal>
+// 		</div>
+// 	)
+// }
+
+export default PokemonModal;
